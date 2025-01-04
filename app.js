76a2ec7e557ebda98bb8e6ba231e06b1c5d211fa
mwendwa-g <geraldmwendwa72@gmail.com>
2025-01-04 17:14:39 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 //contact buttn
 function makeCall() {
     // Replace this with the phone number you want to dial
-    var phoneNumber = "tel:+1234567890";
+    var phoneNumber = "tel:+254110125463";
     window.location.href = phoneNumber;  // This will trigger the device's dialer
 }
