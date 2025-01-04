812ed6f6581668506e3e8554c04bd9d0ccc22bb9
mwendwa-g <geraldmwendwa72@gmail.com>
2025-01-04 14:13:47 +0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,6 @@
+//contact buttn
+function makeCall() {
+    // Replace this with the phone number you want to dial
+    var phoneNumber = "tel:+1234567890";
+    window.location.href = phoneNumber;  // This will trigger the device's dialer
+}
